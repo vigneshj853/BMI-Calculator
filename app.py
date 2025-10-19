6eab473ad4755e7cc5a5740a3783e801d8565ae9
Vignesh <vigneshjayaraj05@gmail.com>
2025-10-19 22:52:41 +0530

    Initial commit- BMI Calculator using Flask

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def bmi_calculator():
+    bmi = None
+    category = None
+
+    if request.method == 'POST':
+        height = float(request.form['height'])
+        weight = float(request.form['weight'])
+
+        # Convert height from cm to meters
+        height_m = height / 100
+        bmi = round(weight / (height_m ** 2), 2)
+
+        # Determine category
+        if bmi < 18.5:
+            category = 'Underweight'
+        elif 18.5 <= bmi < 24.9:
+            category = 'Normal'
+        elif 25 <= bmi < 29.9:
+            category = 'Overweight'
+        else:
+            category = 'Obese'
+
+    return render_template('index.html', bmi=bmi, category=category)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
